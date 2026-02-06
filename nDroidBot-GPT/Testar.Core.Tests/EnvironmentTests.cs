ee465ea577c52fad0b57e12eb2b2ba27f5a9c090
philscrace2 <philscrace@gmail.com>
2026-02-06 11:54:56 +0000

    Fix ambiguous reference to Environment

--- nDroidBot-GPT/Testar.Core.Tests/EnvironmentTests.cs
+++ nDroidBot-GPT/Testar.Core.Tests/EnvironmentTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
 using org.testar.environment;
+using TestarEnvironment = org.testar.environment.Environment;
 
 namespace Testar.Core.Tests
 {
@@ -10,7 +11,7 @@ namespace Testar.Core.Tests
         [Test]
         public void GetDefaultInstanceReturnsUnknown()
         {
-            IEnvironment env = Environment.getInstance();
+            IEnvironment env = TestarEnvironment.getInstance();
             Assert.That(env, Is.Not.Null);
             Assert.That(env, Is.InstanceOf<UnknownEnvironment>());
             Assert.That(env.GetDisplayScale(0L), Is.EqualTo(1.0).Within(0.0001));
@@ -20,16 +21,16 @@ namespace Testar.Core.Tests
         public void SetInstanceStoresValue()
         {
             IEnvironment testEnv = new TestEnvironment();
-            Environment.setInstance(testEnv);
+            TestarEnvironment.setInstance(testEnv);
 
-            IEnvironment result = Environment.getInstance();
+            IEnvironment result = TestarEnvironment.getInstance();
             Assert.That(result, Is.SameAs(testEnv));
         }
 
         [Test]
         public void SetNullInstanceThrows()
         {
-            Assert.That(() => Environment.setInstance(null!), Throws.ArgumentException);
+            Assert.That(() => TestarEnvironment.setInstance(null!), Throws.ArgumentException);
         }
 
         private sealed class TestEnvironment : IEnvironment
