454c0cd115f9a2cf58a4502968d63b25d056b553
philscrace2 <philscrace@gmail.com>
2026-02-06 11:34:02 +0000

    Fix FakeCanvas inheritance problem

--- nDroidBot-GPT/Testar.Core.Tests/TextVisualizerTests.cs
+++ nDroidBot-GPT/Testar.Core.Tests/TextVisualizerTests.cs
@@ -105,12 +105,77 @@ namespace Testar.Core.Tests
                 this.metrics = metrics;
             }
 
+            public double width()
+            {
+                return 0;
+            }
+
+            public double height()
+            {
+                return 0;
+            }
+
+            public double x()
+            {
+                return 0;
+            }
+
+            public double y()
+            {
+                return 0;
+            }
+
+            public void begin()
+            {
+            }
+
+            public void end()
+            {
+            }
+
+            public void line(Pen pen, double x1, double y1, double x2, double y2)
+            {
+            }
+
             public Pair<double, double>? textMetrics(Pen pen, string text)
             {
                 return metrics;
             }
 
             public void text(Pen pen, double x, double y, double angle, string text)
+            {
+            }
+
+            public void clear(double x, double y, double width, double height)
+            {
+            }
+
+            public void triangle(Pen pen, double x1, double y1, double x2, double y2, double x3, double y3)
+            {
+            }
+
+            public void image(Pen pen, double x, double y, double width, double height, int[] image, int imageWidth, int imageHeight)
+            {
+            }
+
+            public void ellipse(Pen pen, double x, double y, double width, double height)
+            {
+            }
+
+            public void rect(Pen pen, double x, double y, double width, double height)
+            {
+            }
+
+            public Pen defaultPen()
+            {
+                return Pen.PEN_BLACK;
+            }
+
+            public void release()
+            {
+            }
+
+            public void paintBatch()
             {
             }
         }
