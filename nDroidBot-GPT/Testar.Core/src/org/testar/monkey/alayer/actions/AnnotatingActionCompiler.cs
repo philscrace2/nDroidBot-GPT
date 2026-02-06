771ca77701e63d6ab8d98d9fc7808f2f9ee28fb4
philscrace2 <philscrace@gmail.com>
2026-02-06 18:21:59 +0000

    Fix AnnotatingActionCompiler

--- nDroidBot-GPT/Testar.Core/src/org/testar/monkey/alayer/actions/AnnotatingActionCompiler.cs
+++ nDroidBot-GPT/Testar.Core/src/org/testar/monkey/alayer/actions/AnnotatingActionCompiler.cs
@@ -1,6 +1,37 @@
+using org.testar.monkey.alayer;
+
 namespace org.testar.monkey.alayer.actions
 {
-    public class AnnotatingActionCompiler
+    public class AnnotatingActionCompiler : StdActionCompiler
     {
+        public Action leftClickAt(Widget widget)
+        {
+            var action = new NOP();
+            action.mapOriginWidget(widget);
+            return action;
+        }
+
+        public Action clickTypeInto(Widget widget, string textToType, bool replaceText)
+        {
+            var action = new NOP();
+            action.mapOriginWidget(widget);
+            action.set(Tags.InputText, textToType);
+            return action;
+        }
+
+        public Action pasteTextInto(Widget widget, string textToPaste, bool replaceText)
+        {
+            var action = new NOP();
+            action.mapOriginWidget(widget);
+            action.set(Tags.InputText, textToPaste);
+            return action;
+        }
+
+        public Action slideFromTo(AbsolutePosition from, AbsolutePosition to, Widget widget)
+        {
+            var action = new NOP();
+            action.mapOriginWidget(widget);
+            return action;
+        }
     }
 }
